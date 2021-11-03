910c9b6a53ad6641208f4d76a623ff9c94f7db73
Yannick Porto <yporto@pryntec.com>
2021-11-03 15:55:59 +0100

    openvino model loader class template

---
+++ src/model_loaders/model_loader_openvino.cpp
@@ -0,0 +1,17 @@
+#include "model_loader_openvino.h"
+
+ModelLoaderOpenVino::ModelLoaderOpenVino()
+{
+}
+
+ModelLoaderOpenVino::~ModelLoaderOpenVino()
+{
+}
+
+void ModelLoaderOpenVino::Load(const std::string &/*modelPath*/)
+{
+}
+
+void ModelLoaderOpenVino::Execute(const std::string &/*imgPath*/)
+{
+}
---
+++ src/model_loaders/model_loader_openvino.h
@@ -0,0 +1,26 @@
+#ifndef MODEL_LADER_OPENVINO_H
+#define MODEL_LADER_OPENVINO_H
+
+#include "model_loader.h"
+
+class ModelLoaderOpenVino : public ModelLoader
+{
+public:
+    ModelLoaderOpenVino();
+    ~ModelLoaderOpenVino();
+
+    /**
+     * @brief load a model from a given path
+     *
+     * @param modelPath path to the model to load
+     */
+    void Load(const std::string &modelPath) override;
+
+    /**
+     * @brief run inference on an image given its path
+     *
+     * @param imgPath path to the image
+     */
+    void Execute(const std::string &imgPath);
+};
+#endif // MODEL_LADER_OPENVINO_H
